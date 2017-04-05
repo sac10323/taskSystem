4a052969ab4f1a962663b3735e2901c55041a71f
shogo_saito <shogo_saito@DXDGPY1>
2017-04-05 17:01:51 +0900

    コマンドラインの処理がおかしくなっていたので修正。

--- src/jp/co/sac/routineTaskSystem/main/TaskSystem.java
+++ src/jp/co/sac/routineTaskSystem/main/TaskSystem.java
@@ -1,5 +1,6 @@
 package jp.co.sac.routineTaskSystem.main;
 
+import java.util.Arrays;
 import jp.co.sac.routineTaskSystem.config.GeneralConfig;
 import jp.co.sac.routineTaskSystem.flow.DocumentCheckFlow;
 import jp.co.sac.routineTaskSystem.flow.IMUpdateRosterFlow;
@@ -15,16 +16,23 @@ public class TaskSystem {
     private static int exitStatus = 0;
 
     public static void main(String[] args) throws Exception {
-        String mode = (args != null && args.length > 0) ? args[0] : null;
+        String mode = "";
+        String[] filePaths = new String[0];
+        if (args != null && args.length > 0) {
+            mode = args[0];
+            filePaths = Arrays.copyOfRange(args, 1, args.length);
+        }
+        
         switch (mode) {
             case "AutoIMCsv": {
                 IMUpdateRosterFlow flow = new IMUpdateRosterFlow();
-                exitStatus = flow.onCheck(args);
+                exitStatus = flow.onCheck(filePaths);
                 break;
             }
+            case "DocCheck":
             default: {
                 DocumentCheckFlow flow = new DocumentCheckFlow();
-                exitStatus = flow.onCheck(args);
+                exitStatus = flow.onCheck(filePaths);
                 break;
             }
         }
